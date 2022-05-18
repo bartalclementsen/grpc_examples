f44c8adc5fff85c892f9c421d0b976e3351648d3
Bartal Clementsen <bartalclementsen@hotmail.com>
2022-05-18 13:44:40 +0100

    GRPC web

--- src/GrpcExamples.Server/Program.cs
+++ src/GrpcExamples.Server/Program.cs
@@ -2,8 +2,14 @@ using GrpcExamples.Server.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+builder.Services.AddCors(o => o.AddPolicy("AllowAll", builder =>
+{
+    builder
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding");
+}));
 
 // Add services to the container.
 builder.Services.AddGrpc();
@@ -12,13 +18,18 @@ builder.Services.AddGrpcReflection();
 var app = builder.Build();
 IWebHostEnvironment env = app.Environment;
 
-// Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+app.UseCors();
+
+app.UseGrpcWeb();
 
 if (env.IsDevelopment())
 {
     app.MapGrpcReflectionService();
 }
+
+app.MapGrpcService<GreeterService>()
+    .EnableGrpcWeb()
+    .RequireCors("AllowAll");
 
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
--- src/GrpcExamples.SpaWebClient/Program.cs
+++ src/GrpcExamples.SpaWebClient/Program.cs
@@ -1,4 +1,8 @@
+using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
+using GrpcExamples.Server;
 using GrpcExamples.SpaWebClient;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +11,14 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddSingleton(services =>
+{
+    var httpClient = new HttpClient(new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler()));
+    var baseUri = "https://localhost:7044";
+    var channel = GrpcChannel.ForAddress(baseUri, new GrpcChannelOptions { HttpClient = httpClient });
+
+    return new Greeter.GreeterClient(channel);
+});
 
 await builder.Build().RunAsync();
